3987da65d64beb9e8ffb5984fda685de13f183a0
Kelvedler <hnflkmvjcm@gmail.com>
2023-05-22 17:10:14 +0300

    fix rabbit url

--- src/app_core/rabbitmq.py
+++ src/app_core/rabbitmq.py
@@ -1,13 +1,15 @@
 import json
 import pika
 
+from app_core.config import config
+
 
 def broadcast_comment(comment, event):
     body = json.dumps({
         'event': event,
         'payload': json.loads(comment)
     })
-    connection = pika.BlockingConnection(pika.ConnectionParameters('127.0.0.1'))
+    connection = pika.BlockingConnection(pika.URLParameters(config.AMQP_URL))
     comments_channel = connection.channel()
     comments_channel.queue_declare(queue='broadcast', auto_delete=True)
     comments_channel.basic_publish(exchange='message', routing_key='broadcast', body=body)
