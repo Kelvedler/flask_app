93322958adc6213723e039a2d16bb3525a9ec7c0
Kelvedler <hnflkmvjcm@gmail.com>
2023-06-21 17:26:45 +0300

    fix nginx header

--- src/gunicorn.conf.py
+++ src/gunicorn.conf.py
@@ -1,4 +1,4 @@
 bind = "0.0.0.0:8000"
-x_forwarded_for_header = "X-Real-IP"
+x_real_ip_header = "X-Real-IP"
 accesslog = '-'
-access_log_format = '%(t)s %({x-forwarded-for}i)s %(l)s "%(r)s" %(s)s "%(a)s"'
+access_log_format = '%(t)s %({x-real-ip}i)s %(l)s "%(r)s" %(s)s "%(a)s"'
