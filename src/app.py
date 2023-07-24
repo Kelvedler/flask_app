eef7a5db59a7f054ec27b3c59d3887130bc7c675
Kelvedler <hnflkmvjcm@gmail.com>
2023-07-24 14:47:32 +0300

    fix 404 handler

--- src/app.py
+++ src/app.py
@@ -1,10 +1,9 @@
 import logging
-import time
 from logging.config import dictConfig
 from flask import Flask
 from pathlib import Path
 
-from app_core.api import BadRequest, ServerInternalError
+from app_core.api import BadRequest, ServerInternalError, NotFound
 from app_core.celery_ import celery_init_app
 from app_core.config import config
 from app_core.db import engine
@@ -26,6 +25,7 @@ def create_app():
         app.register_blueprint(main_api)
 
         app.register_error_handler(400, BadRequest().as_error_handler)
+        app.register_error_handler(404, NotFound().as_error_handler)
         app.register_error_handler(500, ServerInternalError().as_error_handler)
 
         engine.connect()
